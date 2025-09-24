001c01abac76422d2582c0f69ff36d3f230978f8
vivekworldindia <vivekchoudharyup9120@gmail.com>
2025-09-24 05:41:46 +0530

    compare with cs50

---
+++ compare.py
@@ -0,0 +1,12 @@
+x = int(input("What's x?"))
+y= int(input("What's y?"))
+
+
+if x<y:
+    print("x less than y")
+    
+elif x>y :
+    print("x greater than y")
+    
+elif x==y:
+    print("x equals y")
