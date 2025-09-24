218a6bad1920403315bb5a04af15f06d3274a29a
vivekworldindia <vivekchoudharyup9120@gmail.com>
2025-09-24 06:48:22 +0530

    def function use

--- parity.py
+++ parity.py
@@ -1,6 +1,15 @@
-x = int(input("What's x? "))
+def main():
+    x = int(input("What's x? "))
+    if is_even(x):
+        print("Even")
+    else :
+        print("Odd")
+        
+def is_even(n):
+    if n%2 ==0:
+        return True
+    else:
+        return False
 
-if x %2==0:
-    print("Even")
-else:
-    print("Odd")
+
+main()
