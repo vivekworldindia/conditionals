18e6723c46b5771dc303c75a417f4c4c6b06e5d3
vivekworldindia <vivekchoudharyup9120@gmail.com>
2025-09-24 07:12:37 +0530

    house function

---
+++ house.py
@@ -0,0 +1,12 @@
+name = input("What's your name ? ").title()
+
+if name == "Harry" or name == "Hermine" or name =="Ron":
+    print("Gryffindor")
+# elif name == "Hermione":
+#     print("Gryffindor")
+# elif name =="Ron":
+#     print("Gryffindor")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Who?")
