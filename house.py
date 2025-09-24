e2a30e3cd2aee129e9801ece9fc29edd6424e5da
vivekworldindia <vivekchoudharyup9120@gmail.com>
2025-09-24 07:18:57 +0530

    match function

--- house.py
+++ house.py
@@ -1,12 +1,24 @@
-name = input("What's your name ? ").title()
+name = input("What's your name ? ")
 
-if name == "Harry" or name == "Hermine" or name =="Ron":
-    print("Gryffindor")
-# elif name == "Hermione":
+# if name == "Harry" or name == "Hermine" or name =="Ron":
 #     print("Gryffindor")
-# elif name =="Ron":
-#     print("Gryffindor")
-elif name == "Draco":
-    print("Slytherin")
-else:
-    print("Who?")
+# # elif name == "Hermione":
+# #     print("Gryffindor")
+# # elif name =="Ron":
+# #     print("Gryffindor")
+# elif name == "Draco":
+#     print("Slytherin")
+# else:
+#     print("Who?")
+
+match name :
+    case "Harry" | "Hermine" | "Ron":
+        print("Gryffindor")
+    # case 'hermine':
+    #     print("Gryffindor")
+    # case "Ron":
+    #     print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+            print("Who?")
